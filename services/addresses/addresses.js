b8d26cd0624cf2113991d6abc374d24e71e26023
ToxaKZ <Anton.Zorya@gmail.com>
2015-09-21 17:40:52 +0600

    Routing for service address

--- services/addresses/addresses.js
+++ services/addresses/addresses.js
@@ -5,8 +5,18 @@ var clientFactory = require("devir-mbclient");
 
 var client = new clientFactory.core(clientFactory.netConnector, "localhost", "9009", function(isReconecting){
 
-console.log("Подключение установлено");
+    console.log("Подключение установлено");
+
+    client.registerRoute("/address/getAllByParentId", function(request){
+        
+    });
 
 
+    client.registerRoute("/address/addAddressNode", function(request){
+
+    });
+
+
+    client.registerService();
 
 });
