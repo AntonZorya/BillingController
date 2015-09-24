17f700ed2d6055a1545c89b481cf08061708930d
nursultan156 <nursultan156@gmail.com>
2015-09-24 15:56:49 +0600

    sqlBuilder fix

--- services/common/sql/sqlBuilder.js
+++ services/common/sql/sqlBuilder.js
@@ -4,17 +4,19 @@
 module.exports = function(sql, params){
     if(params)
     params.forEach(function(item){
-       sql = sql.replace(":"+item.key, itemValueBuilder(item.value));
-        sql = sql.replace("@"+item.key, item.value);
+        sql = sql.replace(":"+item.key, itemValueBuilder(item.value, true));
+        sql = sql.replace("@"+item.key, itemValueBuilder(item.value, false));
     });
 
     return sql;
-}
+};
 
-var itemValueBuilder = function(value) {
+var itemValueBuilder = function(value, quot) {
     if (typeof value == "string") {
-        var s = value.replace(/"/g, "\\\"");
+        var s = value.replace(/\\/g, "\\\\");
         s = s.replace(/'/g, "\\\'");
-        return "\"" + s + "\"";
+        s = s.replace(/"/g, "\\\"");
+        if(quot) return "\"" + s + "\"";
+        else return s;
     } else return value;
-}
+};
