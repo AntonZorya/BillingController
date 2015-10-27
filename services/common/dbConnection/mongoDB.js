dbe747cc14ee1a05e08dc9be23f627e05b98161e
nursultan156 <nursultan156@gmail.com>
2015-10-27 11:34:52 +0600

    loadings db connection

--- services/common/dbConnection/mongoDB.js
+++ services/common/dbConnection/mongoDB.js
@@ -2,7 +2,7 @@
  * Created by vaio on 26.09.2015.
  */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.66.27/BillingControllerAddress');
+mongoose.connect('mongodb://192.168.66.27/BillingController');
 
 //var conn = mongoose.connection[0];
 
